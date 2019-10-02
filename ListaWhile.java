088004465d78beea0411525e3ec1e704be8ef8dd
Ascendina <camilaascendina@gmail.com>
2019-10-02 16:42:25 -0300

    Exercício While - respostas

---
+++ ListaWhile.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+
+public class ListaWhile {
+
+	public static void main(String[] args) {
+		/* ************************************
+		 * *************Questão 1**************
+		 * *************************************/
+		
+		/*int contador = 0;
+		
+		while(contador <= 10){
+			contador = contador + 1;
+			System.out.println(contador);
+		}*/
+		
+		
+		/* ************************************
+		 * *************Questão 2**************
+		 * *************************************/
+		
+		/*Scanner console = new Scanner (System.in);
+		
+		System.out.println("Digite um número: ");
+		int num = console.nextInt();
+		
+		int contador = 0;
+		int soma = 0;
+		
+		while(contador <= num){
+			soma = soma + contador;
+			
+			contador = contador + 1;
+			
+		}
+		
+		System.out.println("Média: " + (soma/contador));*/
+		
+		
+		/* ************************************
+		 * *************Questão 3**************
+		 * *************************************/
+		
+		
+		/*Scanner console = new Scanner (System.in);
+		
+		System.out.println("Digite uma temperatura: ");
+		float temp = console.nextFloat();
+		
+		float soma = 0;
+		float maiorTemp = 0;
+		float menorTemp = 100000;
+		int contador = 0;
+		
+		while(temp != -1000){
+			//maior temperatura
+			if(temp > maiorTemp){
+				maiorTemp = temp;
+			}
+			
+			//Menor temperatura
+			if(temp < menorTemp){
+				menorTemp = temp;
+			}
+			
+			//Somando as temperaturas
+			soma = soma + temp;
+			
+			//contando a quantidade de elementos
+			contador = contador + 1;
+			
+			//Perguntando ao usuário a proxima temperatura
+			System.out.println("Digite uma temperatura: ");
+			temp = console.nextFloat();
+		}
+		
+		System.out.println("Maior temperatura: " + maiorTemp);
+		System.out.println("Menor temperatura: " + menorTemp);
+		System.out.println("Média das temperaturas: " + (soma/contador));
+		*/
+		
+		
+		
+
+	}
+
+}
